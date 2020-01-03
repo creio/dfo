d5a81e66b3ecd6ca547ea0bf4092929f0beae120
creio <mailcreio@gmail.com>
2020-01-03 07:17:59 +0300

    Deploy updates

--- gh_deploy.sh
+++ gh_deploy.sh
@@ -5,20 +5,20 @@ build_command() {
   jekyll build
 }
 
-echo -e "\033[0;32mDeleting old content...\033[0m"
+echo -e "Deleting old content"
 rm -rf $directory
 
-echo -e "\033[0;32mChecking out $branch....\033[0m"
+echo -e "Checking out $branch"
 git worktree add $directory $branch
 
-echo -e "\033[0;32mGenerating site...\033[0m"
+echo -e "Generating site"
 build_command
 
-echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+echo -e "Deploying $branch branch"
 cd $directory &&
   git add --all &&
   git commit -m "Deploy updates" &&
   git push origin $branch
 
-echo -e "\033[0;32mCleaning up...\033[0m"
+echo -e "Cleaning up"
 git worktree remove $directory
